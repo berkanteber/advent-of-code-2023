56fe1a2d1caa4d3c0e19a70c4c4c35e683935d50
Berkan Teber <berkan@berkanteber.com>
2023-12-14 22:16:48 +0300

    Add day 9

---
+++ day_09/part_1.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import os
+from itertools import pairwise
+
+
+INPUT_PATH = os.path.join(os.path.dirname(__file__), 'input.txt')
+with open(INPUT_PATH) as f:
+    INPUT_DATA = f.read()
+
+
+def solve(data: str) -> int:
+    total = 0
+    for line in data.splitlines():
+        nums = list(map(int, line.split()))
+
+        subnums = [nums]
+        while not all(n == 0 for n in subnums[-1]):
+            subnums.append([])
+            for n1, n2 in pairwise(subnums[-2]):
+                subnums[-1].append(n2 - n1)
+
+        total += sum(sn[-1] for sn in subnums if sn)
+
+    return total
+
+
+TEST_DATA = """\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+"""
+
+assert solve(TEST_DATA) == 114
+
+print(solve(INPUT_DATA))  # 1789635132
